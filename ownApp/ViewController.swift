6d965ed1b6e64abf48078ef8c1e67198ae7b210c
shapaj <domenik2005shapaj@gmail.com>
2021-08-06 21:27:49 +0300

    Initial Commit

---
+++ ownApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ownApp
+//
+//  Created by Ihor Shovkoplias on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
